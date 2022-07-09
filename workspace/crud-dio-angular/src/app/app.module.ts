82ef6d9018470e017f063bb26ba29d7989f81b52
Fernando <fernando.luiz92@hotmail.com>
2022-07-09 09:25:51 -0300

    Criando a interface e service course

--- workspace/crud-dio-angular/src/app/app.module.ts
+++ workspace/crud-dio-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { CourseFormComponent } from './pages/course/course-form/course-form.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
